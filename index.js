0e7605fd8219ea8ab8d6109dcdc982c9e4761163
Derrbal <bderbal07@gmail.com>
2022-08-28 04:41:25 -0400

    added cli-welcome

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log(`Doregarray`);
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Get to know Doregarray`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+});
+
+console.log(`Doregarray!`);
